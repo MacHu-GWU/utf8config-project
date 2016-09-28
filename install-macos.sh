2c9d83cc8691cf1a92b20715e8110726fdcd45fe
MacHu-GWU <husanhe@gmail.com>
2016-09-28 16:47:17 -0400

    2016-09-28
    First release

---
+++ install-macos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+cd utf8config
+python zzz_manual_install.py
---
+++ tests/test_all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+print("是".lower() in ["是"])
+# if __name__ == "__main__":
+#     import pytest
+#     pytest.main(["--tb=native", "-s"])
